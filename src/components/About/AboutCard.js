8a0c2bdce65c297e0136c1ee58e201e935b98a5a
high5dev <high5dev621@hotmail.com>
2024-06-25 09:52:59 -0400

    some changes in about me

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,7 +8,15 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hello, I am <span className="purple">Cory Mack</span>, 
-            a passionate and experienced <span className="purple">Full Stack Web Developer</span> with over 10 years of experience in building dynamic and responsive web applications. 
+            and I am a seasoned <span className="purple">software engineer</span> and <span className="purple">full-stack developer</span>
+            with over a decade of experience in crafting high-quality, scalable, and user-centric web applications. 
+            <br />
+            I hold a Bachelor’s degree in Computer Science from Franklin University, 
+            and my technical skill set includes a diverse range of programming languages and frameworks, 
+            such as JavaScript, TypeScript, Python, PHP, C#, and Go, to name a few.
+            <br /> 
+            I am also proficient in various databases and front-end and back-end technologies, 
+            making me a well-rounded developer capable of tackling complex challenges across the stack.
             <br />
             {/* Senior software Engineer who loves to transform ideas into reality using
             code.
